fb9436c35229ede9215ef54f9a772a10ae1fbb76
Mohd Nawaz <nawazthezaifre@gmail.com>
2025-09-13 20:00:49 +0530

    fallback page:

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'dynamic-cache';
 const assets = [
   './',
   './index.html', //correct
@@ -7,7 +8,11 @@ const assets = [
   './js/materialize.min.js', //corr
   './css/styles.css', //corr
   './css/materialize.min.css', //corr
-  './img/dish.png'//corr 
+  './img/dish.png', //corr 
+  './pages/fallback.html',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v144/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+  
 ];
 
 self.addEventListener('install', evt => {
@@ -37,7 +42,7 @@ self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.filter(key => key !== staticCacheName).map(key => caches.delete(key))
+        keys.filter(key => key !== staticCacheName && key !== dynamicCacheName).map(key => caches.delete(key))
       )
     })
   );
@@ -48,8 +53,18 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   evt.respondWith(
   caches.match(evt.request).then(cachesMatch => {
-    return cachesMatch || fetch(evt.request); //if it is matching we will return the match otherwise normal fetch request that user requested
+    //if it is matching we will return the match otherwise normal fetch request that user requested
+    return cachesMatch || fetch(evt.request)
+    //niche ka code basically ye kar rha jab home online honge agar ham offline hone se pehle visit karenge to work nhi karega
+    //Otherwise 404 error aayegi
+    
+    // .then(fetchRespon => {
+    //   return caches.open(dynamicCacheName).then(cache => {
+    //     cache.put(evt.request.url, fetchRespon)
+    //     return fetch(evt.request);
+    //   })
+    // }) 
 
-  })
+  }).catch(() => caches.match('./pages/fallback.html')) //ye miss kar rha tha fallback me error catch se hi chalega
 )
 });
